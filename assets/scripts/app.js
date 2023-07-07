5758bc03332d3a391fbfaf5f29d6757ad5c6a639
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-07-07 15:49:44 +0530

    init

---
+++ assets/scripts/app.js
@@ -0,0 +1,21 @@
+const countdown = () => {
+    const countDate = new Date("July 7, 2023 17:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const seconds = 1000;
+    const minutes = seconds * 60;
+    const hours = minutes * 60;
+    const days = hours * 24;
+
+    const textDay = Math.floor(gap / days);
+    const textHour = Math.floor((gap % days) / hours);
+    const textMinute = Math.floor((gap % hours) / minutes);
+    const textSecond = Math.floor((gap % minutes) / seconds);
+
+    document.getElementById("hour").innerText = textHour;
+    document.getElementById("minute").innerText = textMinute;
+    document.getElementById("second").innerText = textSecond;
+};
+
+setInterval(countdown, 1000);
